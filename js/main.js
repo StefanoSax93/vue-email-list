22b73c5a00e5547b30b9a4b3415ff84c22087327
StefanoSax93 <101599907+StefanoSax93@users.noreply.github.com>
2022-06-17 23:47:39 +0200

    modifica mounted

--- js/main.js
+++ js/main.js
@@ -16,7 +16,7 @@ const appVue = new Vue({
         listaEmail: [],
     },
 
-    mounted() {
+    created() {
         for (let i = 1; i <= 10; i++) {
             axios
                 .get('https://flynn.boolean.careers/exercises/api/random/mail')
